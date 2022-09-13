404bbb58ec278c4012d993972561f8fb207d1ddf
root <gumedegeorge6@gmail.com>
2022-09-13 12:46:36 +0200

    This is commit for 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "_putchar.c"
+
 /**
  *main - Prints "Putchar into the standard output"
  *
  *Return: 0-always
  */
 int main(void)
+
 {
-    char string[] = "_putchar";/*Declare the array and initialise it*/
-    int j =0;
-    for(j = 0; j<9 ; j++)
-    {
-        _putchar(string[j]);
-    }
-    _putchar('\n');
+char string[] = "_putchar";/*Declare the array and initialise it*/
+int j = 0;
+for (j = 0; j < 9 ; j++)
+{
+_putchar(string[j]);
+}
+
+_putchar('\n');
+
+return (0);
 
 }
