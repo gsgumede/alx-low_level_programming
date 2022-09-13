b1548141c6f16bfe425260d7e289736b03d48f47
George Gumede <gumedegeorge6@gmail.com>
2022-09-13 18:21:18 +0200

    This commit for 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints 10x the alphabet in lowercase
+ *
+ *Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+int i;/*Declare initial value in inner loop*/
+int j;/*Declare initial value for outer loop*/
+
+char alphabet = 'a';/*Declare and initialise the first alphabet*/
+
+/*alphabet loop*/
+for (i = 0; i <= 10; i++)
+{
+for (j = alphabet; j <= 'z'; j++)
+{
+_putchar(j);/*Display each alphabet*/
+}
+_putchar('\n');/*New line*/
+}
+
+return;
+
+}
