877a4cd596b8a23aba626bf1b3f08e51a173995d
George Gumede <gumedegeorge6@gmail.com>
2022-09-14 10:29:27 +0200

    This commit is for 4-islapha.c

---
+++ 0x02-functions_nested_loops/4-islapha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isalpha - check alphabetic character
+ *
+ *@c: The int of the alphabet
+ *Return: 1-if alphabet, 0-if not
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))/*check if alphabet*/
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
