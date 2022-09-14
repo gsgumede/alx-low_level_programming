a7d41f4b706b46d6c5cc19d457d0f81efa3acf16
George Gumede <gumedegeorge6@gmail.com>
2022-09-14 11:22:57 +0200

    This commit is for 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Prints the last digit of a number
+ *
+ *@n: The number
+ *Return: Value of the last digit
+ */
+int print_last_digit(int n)
+{
+int remainder = n % 10;/*Get the remainder of an integer when divided by 10*/
+return (remainder);
+}
