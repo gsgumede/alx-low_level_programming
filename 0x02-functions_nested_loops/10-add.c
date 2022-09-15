79e3bd5eba43488a078920829a9ac67c92388082
George Gumede <gumedegeorge6@gmail.com>
2022-09-15 06:19:09 +0200

    This commit is for 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - adds two integers
+ *
+ * @n: Integer to be added
+ * @k: Integer to be added
+ * Return: Sum of the integers
+ */
+int add(int n, int k)
+{
+int sum = n + k;
+return (sum);
+}
