54ac693005dd7160b54ba9b54199a9f8ee9194db
George Gumede <gumedegeorge6@gmail.com>
2022-09-16 06:42:20 +0200

    This commit is for 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiply two given integers
+ *
+ * @a: First given integer
+ * @b: Second given integer
+ *
+ * Return: Return product of two ints
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
