ae7d93634f42541df33a869b33aa66254aba8a15
George Gumede <gumedegeorge6@gmail.com>
2022-09-16 10:10:16 +0200

    This commit is for 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isdigit - Checks for a digit (0 through 9)
+ *
+ *@c: The digit
+ *Return: 1-if digit, 0-if not digit
+ */
+int _isdigit(int c)
+{
+
+if (c <= 58 && c >= 48)/*Start if statement*/
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
