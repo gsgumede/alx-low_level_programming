f43dc5b03749f85b036c09566c2c49c437fa5529
George Gumede <gumedegeorge6@gmail.com>
2022-09-29 02:20:18 +0200

    Solution for 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+* print_chessboard - prints the chessboard
+*
+* @a: pointer to char
+* Return: nothing
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	for (; i <= 8; i++)
+	{
+		for (; j <= 8; j++)
+		{
+			_putchar(*(a[i] + j);
+		}
+	}
+}
