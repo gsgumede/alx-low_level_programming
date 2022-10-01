d712fa641fc6698a9dbdb3d35fa86f6f1ac4325a
George Gumede <gumedegeorge6@gmail.com>
2022-10-01 09:51:11 +0200

    commit for 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,33 +22,39 @@ int main(int argc, char *argv[])
 
 	cents = atoi(argv[1]);
 
-	while (cents > 0)
+	if (cents <= 0)
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
+		printf("%d\n", 0);
 	}
-
-	printf("%d\n", coins);
+	else
+	{
+		while (cents > 0)
+		{
+			coins++;
+			if ((cents - 25) >= 0)
+			{
+				cents -= 25;
+				continue;
+			}
+			if ((cents - 10) >= 0)
+			{
+				cents -= 10;
+				continue;
+			}
+			if ((cents - 5) >= 0)
+			{
+				cents -= 5;
+				continue;
+			}
+			if ((cents - 2) >= 0)
+			{
+				cents -= 2;
+				continue;
+			}
+			cents--;
+		}
+		printf("%d\n", coins);
+	}
 
 	return (0);
 }
