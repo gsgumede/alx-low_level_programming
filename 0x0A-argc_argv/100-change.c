55511305714ae34f78274ef66fdd1a08c0da2dce
George Gumede <gumedegeorge6@gmail.com>
2022-10-01 10:34:12 +0200

    commit for 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int calcul_(int cents);
+
 /**
 * main - prints the minimum number of coins to make a change
 *	 for an amount of money
@@ -12,7 +14,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int cents;
 
 	if (argc != 2)
 	{
@@ -28,34 +30,55 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		while (cents > 0)
-		{
-			coins++;
-			if ((cents - 25) >= 0)
-			{
-				cents -= 25;
-				continue;
-			}
-			if ((cents - 10) >= 0)
-			{
-				cents -= 10;
-				continue;
-			}
-			if ((cents - 5) >= 0)
-			{
-				cents -= 5;
-				continue;
-			}
-			if ((cents - 2) >= 0)
-			{
-				cents -= 2;
-				continue;
-			}
-			cents--;
-		}
-		printf("%d\n", coins);
+		printf("%d\n", calcul_(cents));
 	}
 
 	return (0);
+}
+
+/**
+* calcul_ - calculate number of coins needed
+*
+* @cents: cents
+* Return: The number of coins needed
+*/
+int calcul_(int cents)
+{
+	int coins = 0;
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		else if	((cents - 10)  >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		else if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+
+		}
+		else if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		else if ((cents - 1) >= 0)
+		{
+			cents -= 1;
+			continue;
+		}
+		cents--;
+	}
+
+	return (coins);
+
 }
 
