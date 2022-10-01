3ff768ebca7f72fbe9fd2f0223be59ad5d3dad09
George Gumede <gumedegeorge6@gmail.com>
2022-10-01 13:03:42 +0200

    commit for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int check_if_number(int argc, char **argv);
+int check_if_digit(char *num);
+void add_print(int argc, char **argv);
+
 /**
 * main - add two numbers and prints the output
 *
@@ -10,43 +14,108 @@
 */
 int main(int argc, char **argv)
 {
-	int j;
-	int i;
-	int count = 0;
-	int result = 0;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
 		printf("%d\n", 0);
 	}
-
-	for (i = 0; i < argc; i++)
+	else
 	{
-		if (atoi(*(argv + i)) == 0)
+
+		if (check_if_number(argc, argv))
+		{
+			add_print(argc, argv);
+
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+	}
+
+	return (0);
+
+}
+
+/**
+* check_if_digit - check if a number
+*
+* @num: string to be checked
+* Return: 1 - if number, 0 if not number
+*/
+int check_if_digit(char *num)
+{
+	int j;
+	int count = 0;
+
+	for (j = 0; *(num + j) != '\0'; j++)
+	{
+		if (*(num + j) >= '0' && *(num + j) <= '9')
 		{
 			count++;
 		}
 	}
 
-	if (count > 1)
+	if (count == j)
 	{
-
-		printf("Error\n");
 		return (1);
-
 	}
-
-	else if (argc > 1 )
+	else
 	{
-		for (j = 0; j < argc; j++)
-		{
-			result += atoi(*(argv + j));
+		return (0);
+	}
+}
 
-		}
-		 printf("%d\n", result);
+/**
+* add_print - add the numbers and  prints result
+*
+* @argv: arguments string
+* @argc: number of arguments
+* Return: void
+*/
+void add_print(int argc, char **argv)
+{
+	int result = 0;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		result += atoi(*(argv + i));
 	}
 
-	return (0);
+	printf("%d\n", result);
+}
 
+/**
+* check_if_number - check if number
+*
+* @argc: number of arguments
+* @argv: arguments string
+* Return: 1 - if its a number
+*	  0 - if not a number
+*/
+int check_if_number(int argc, char **argv)
+{
+	int i;
+	int count = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (check_if_digit(*(argv + i)))
+		{
+			count++;
+		}
+	}
+
+	if (i == count)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
