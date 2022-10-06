fa5b6c7aa1cd1d40dd413cda198db508a108bb4f
George Gumede <gumedegeorge6@gmail.com>
2022-10-06 13:35:46 +0200

    Add: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,10 +14,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int j = 0, len, len1 = 0, len2 = 0, i;
 	char *concat;
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 		len2++;
-	if (len2 < n)
+	if (len2 >= n)
 	{
 		n = len2;
 	}
