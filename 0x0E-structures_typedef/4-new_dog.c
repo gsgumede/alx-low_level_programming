045ab3a079d87be6d060d9e2cd41a5bb0ffd545c
George Gumede <gumedegeorge6@gmail.com>
2022-10-10 18:13:01 +0200

    Add: 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,45 @@
 #include<stddef.h>
 #include<stdlib.h>
 
+void copy_strin(char *src, char *dst, int len);
+int get_len(char *s);
+
+/**
+ * get_len - calculates the length of the string
+ *
+ * @s: string to be calculated
+ * Return: length of the string
+ */
+int get_len(char *s)
+{
+	int i = 0;
+	
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * copy_strin - copies the contents of the string
+ * 
+ * @src: address of the source string
+ * @dst: address of the destination string
+ * @len: length of the string
+ * Return: Address to the first element in the destination element
+*/
+void copy_strin(char *src, char *dst, int len)
+{
+	int i;
+
+	len++;
+	for (i = 0; i < len; i++)
+	{
+		src[i] = dst[i];
+	}
+}
+
 /**
  * new_dog - creates new dog
  *
@@ -14,15 +53,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_d;
 
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
 	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 		return (NULL);
-	if (name != NULL || owner != NULL)
+	new_d->name = malloc(sizeof(char) * (get_len(name) + 1));
+	new_d->owner = malloc(sizeof(char) * (get_len(owner) + 1));
+	if (d->name == NULL || d->owner == NULL)
 	{
-		new_d->age = age;
-		new_d->name = name;
-		new_d->owner = owner;
-		return (new_d);
-	}
-	return (NULL);
+		free(new_d);
+		return (NULL);
+	}	
+	copy_strin(name, new_d->name, get_len(name));
+	copy_strin(owner, new_d->owner, get_len(name));
+	new_d->age = age;
+	return (new_d);
 }
