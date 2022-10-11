accc9cb2dc9458f932abc4fb67b60fb3c38f4a43
George Gumede <gumedegeorge6@gmail.com>
2022-10-11 13:47:43 +0200

    Add: 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,14 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/*
- * File: 0-object_like_macro.h
- *
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
+#ifndef O_OBJECT_LIKE_MACRO_H
+#define O_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
 #endif
-
