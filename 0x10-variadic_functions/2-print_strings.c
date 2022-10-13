46d087a8c352457d156c88104044dc5a1fa8e774
George Gumede <gumedegeorge6@gmail.com>
2022-10-13 16:21:01 +0200

    Add: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,9 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int num_tmp;
 	char *tmp;
 	va_list strin;
-	
-	if (n == 0)
-		return;
+
 	va_start(strin, n);
 	num_tmp = n;
 	num_tmp--;
