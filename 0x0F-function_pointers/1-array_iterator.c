aca8cf1281fdaf4badf80e4f77602fcc217ee2ef
George Gumede <gumedegeorge6@gmail.com>
2022-10-14 11:41:07 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t index;
 
-	if (array == NULL || size <= 0 || action == NULL)
+	if (array == NULL || size == 0 || action == NULL)
 		return;
 	for (index = 0; index < size; index++)
 	{
 		action(array[index]);
 	}
 }
-
