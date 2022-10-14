ca0b93fd47a84dd3066abf7231a195d33dd21290
George Gumede <gumedegeorge6@gmail.com>
2022-10-14 12:12:50 +0200

    Add: function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
-#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
