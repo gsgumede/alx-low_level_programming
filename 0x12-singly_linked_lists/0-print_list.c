6c35e7b0fe6c8355004dc18655abb63f18579378
George Gumede <gumedegeorge6@gmail.com>
2022-10-22 12:50:13 +0200

    Add: 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,74 +1,91 @@
 #include "lists.h"
-#include<stdlib.h>
-#include<stddef.h>
-
-void print(char *str, unsigned int len);
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+void print_con(char *str, unsigned int len);
 
 /**
-* print_list - print all the elements of a list
-*
-* @h: the pointer to the list
-* Return: The number of nodes.
-*/
+ * print_list - print all elemets of a list
+ *
+ * @h: The list
+ * Return: Number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	const list_t *temp;
+	char *nil;
+	unsigned int count, index;
 
 	if (h == NULL)
-		return (NULL);
+		return (0);
+	nil = "(nil)";
+	count = 0;
 	while (h != NULL)
 	{
-		print (h->str, h->len);
-		h = h->next;
-		count++;
-	}
-}
-/**
-* print - print elements
-*
-* @str: string to be printed
-* @len: len of the string to be printed
-* Return: Nothing
-*/
-void print(char *str, unsigned int len)
-{
-	unsigned int index, count, temp;
-	unsigned int *ptr;
-
-	_putchar('[');
-	count = 0;
-	temp = len;
-	while (temp != 0)
-	{
-		count++;
-		temp / 10;
-	}
-	ptr = malloc(sizeof(int) * count);
-	temp = len;
-	for (index = 0; index < count; index++)
-	{
-		ptr[index] = temp % 10;
-		temp = temp / 10;
-	}
-		_putchar('[');
-		count--;
-		for (index = count; index > 0; index--)
-		_putchar(ptr[index]);
-		_putchar('[');
-		_putchar(32);
-	if (str == NULL)
-	{
-		_putchar('[');
-		_putchar('0' + 0);
-		_putchar(']');
-	}
-	else
-	{
-		while (str)
+		temp = h;
+		index = 0;
+		if (h->str == NULL)
 		{
-		_putchar(*str);
-		str++;
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(32);
+			index = 0;
+			while (nil[index])
+			{
+				_putchar(nil[index]);
+				index++;
+			}
+			_putchar('\n');
 		}
+		else
+		{
+			print_con(h->str, h->len);
+		}
+		h = temp->next;
+		count++;
 	}
+	return (count);
 }
 
+/**
+ * print_con - print the contents
+ *
+ * @str: string to be printed
+ * @len: length of the string.
+ * Return: nothing
+ */
+void print_con(char *str, unsigned int len)
+{
+	unsigned int index, number, count_num;
+	char *digits;
+
+	number = len;
+	index = 1;
+	count_num = 0;
+	while (number)
+	{
+		index++;
+		number = number / 10;
+		count_num++;
+	}
+	digits = malloc(sizeof(char) * count_num);
+	_putchar('[');
+	for (index = 0; index  < count_num; index++)
+	{
+		digits[index] = (len % 10) + '0';
+		len = len / 10;
+	}
+	for (index = 0; index < count_num; index++)
+		_putchar(digits[index]);
+	_putchar(']');
+	_putchar(32);
+	index = 0;
+	while (str[index])
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+	free(digits);
+}
