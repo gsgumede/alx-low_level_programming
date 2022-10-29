968ce8096a62d8194cb14449a686a2093fe3a592
George Gumede <gumedegeorge6@gmail.com>
2022-10-29 18:35:48 +0200

    Update: function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,7 @@
 unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char);
+
 #endif /*MAIN_H*/
