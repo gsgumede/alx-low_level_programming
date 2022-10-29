b2334e6dc30a73e05a20067cc7d69e6efc50eb38
George Gumede <gumedegeorge6@gmail.com>
2022-10-29 18:35:00 +0200

    Add: 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,23 +2,23 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * 
+ *
  * @n: The number used to get the bit
  * @index: The index
  * Return: The value of the bit at index or -1
-*/
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int temp;
+	unsigned long int temp;
 
-    if (index > 64)
-        return (-1);
-    temp = 1;
-    temp <<= index;
-    n &= temp;
-    if (n)
-        return (1);
-    else
-        return (0);
+	if (index > 64)
+		return (-1);
+	temp = 1;
+	temp <<= index;
+	n &= temp;
+	if (n)
+		return (1);
+	else
+		return (0);
 }
 
