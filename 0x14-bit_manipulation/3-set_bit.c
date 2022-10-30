2561d898d51231e4e150ad5b55d4c87818f8bb2b
George Gumede <gumedegeorge6@gmail.com>
2022-10-30 09:42:10 +0200

    Update: 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,6 +15,8 @@ int set_bit(unsigned long int *n, unsigned int index)
     number = 1;
     if (n == NULL)
         return (-1);
+    if (index > 64)
+        return (-1);
     number <<= index;
     *n |= number;
     return (1);
