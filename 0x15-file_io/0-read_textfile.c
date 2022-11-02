dda9c4b2013ec5d56c9ff35b60c6ccc213908210
George Gumede <gumedegeorge6@gmail.com>
2022-11-02 05:52:42 +0200

    Add:all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,31 @@
 #include "main.h"
-#include <stddef.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 /**
- * read_textfile - read a text file and prints it to the POSIX standard output
- *
- * @filename: Pointer to the file name
- * @letters: The number of chars in the file
- * Return: The number of letters it could print or
- *			0 if the file can not be opened or read
- */
-size_t read_textfile(const char *filename, size_t letters)
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int fd;
-	ssize_t read_n, write_n;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buff)
 		return (0);
-	fd = open(filename, O_RDONLY);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-		return (0);
-	read_n = read(fd, buffer, letters);
-	if (read_n == -1)
-		return (0);
-	write_n = write(STDIN_FILENO, buffer, letters);
-	if (write_n == -1)
-		return (0);
-	return (write_n);
+
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
 }
